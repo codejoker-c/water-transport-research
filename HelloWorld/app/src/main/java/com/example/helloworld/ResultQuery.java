1af0dd35f9ad0eba88db9262a8bb5f0e712f1653
RealLiuzheng <884712443@qq.com>
2022-02-28 22:58:26 +0800

    还是尝试...

--- HelloWorld/app/src/main/java/com/example/helloworld/ResultQuery.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/ResultQuery.java
@@ -2,6 +2,7 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.widget.TextView;
@@ -19,6 +20,7 @@ import java.util.List;
 public class ResultQuery extends AppCompatActivity {
 
     ArrayList ship_data;
+    TextView text;
     private WT_ViewModel mWT_ViewModel;//实例化WT_ViewModel来与数据库进行交互
 
     List<user_Boat> BoatData;
@@ -30,7 +32,9 @@ public class ResultQuery extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_query);
 
+        text=findViewById(R.id.test);
 
+        mWT_ViewModel = new ViewModelProvider(this).get(WT_ViewModel.class);
         //拿到船和货的数据
         mWT_ViewModel.getAlluserCargo().observe(this, new Observer<List<user_Cargo>>() {
             @Override
@@ -38,6 +42,7 @@ public class ResultQuery extends AppCompatActivity {
                 CargoData=user_cargos;
             }
         });
+
         mWT_ViewModel.getAlluserBoat().observe(this, new Observer<List<user_Boat>>() {
             @Override
             public void onChanged(List<user_Boat> user_boats) {
@@ -46,7 +51,7 @@ public class ResultQuery extends AppCompatActivity {
         });
 
         initPython();
-
+        callPythonCode(BoatData,CargoData,text);
 
 
     }
@@ -58,21 +63,19 @@ public class ResultQuery extends AppCompatActivity {
         }
     }
     // 调用python代码
-    void callPythonCode(List<user_Boat> BoatData,List<user_Cargo> CargoData){
+    void callPythonCode(List<user_Boat> BoatData,List<user_Cargo> CargoData,TextView textView){
 
         Python py = Python.getInstance();
         // 调用hello.py模块中的greet函数，并传一个参数
         // 等价用法：py.getModule("hello").get("greet").call("Android");
-        py.getModule("hello").callAttr("greet", "Android");
-
-        // 调用python内建函数help()，输出了帮助信息
-        // py.getBuiltins().get("help").call();
 
         //PyObject用来保存python函数的返回值，一个python类型
+        //py.getModule("hello").get("km_code").call(BoatData,CargoData);
         PyObject obj1 = py.getModule("km_code").callAttr("final_func", BoatData,CargoData);
         // 将Python返回值换为Java中的Integer类型
-        Integer sum = obj1.toJava(Integer.class);
-        Test.setText(sum.toString());
+        //Integer sum = obj1.toJava(Integer.class);
+        List<List<Integer>> sum2 = obj1.toJava(List<List<Integer>>.class);
+        textView.setText(sum2[0][0].toString());
 
     }
 
--- HelloWorld/app/src/main/python/km_code.py
+++ HelloWorld/app/src/main/python/km_code.py
@@ -181,86 +181,40 @@ def compute_km(datas, min=False):
         result.append((i, match[i] if match[i] < col else -1))  # 没有对应的值给-1
     return result   # 返回result二维数组，匹配的结果，，一个X，一个Y
 
-def final_func():
-    mydb = db.get_db()
-
-    db.init_db(mydb)
-
-    cur = mydb.cursor()
-
-    cur.execute("SELECT * FROM cargo")
-    info_cargo = cur.fetchall()
-
-    cur.execute("SELECT * FROM ship")
-    info_ship = cur.fetchall()
-
-    # print(type(info_ship))   # list类型
-    # print(type(info_ship[0]))   # sqite_row类型
-    # for i in range(4):
-    #     for key,value in dict(info_ship[i]).items():
-    #         print(key,value)
+def final_func(Ship_info_array,Cargo_info_array):
 
 
-    Loc_Correspond={"重庆":15,"大连":20,"东莞":22,"福州":28,"泰兴":42,"南通":73,"海安":56,"盐城":35,"宣城":5}
+    Loc_Correspond={"Chongqing":15,"Dalian":20,"Dongguan":22,"Fuzhou":28,"Taixing":42,"Nantong":73,"Haian":56,"Yancheng":35,"Xuancheng":5}
 
-    Ship_info_array=cur.execute(
-        'SELECT p.id, loc, M_ship, V_ship, Cost_trans, Cost_live '
-        ' FROM ship p '
-    ).fetchall()
-    Cargo_info_array=cur.execute(
-        'SELECT p.id, depart, destin, m_cargo, target_money '
-        ' FROM cargo p '
-    ).fetchall()
+    #Loc_Correspond={"重庆":15,"大连":20,"东莞":22,"福州":28,"泰兴":42,"南通":73,"海安":56,"盐城":35,"宣城":5}
 
-    #print(Ship_info_array[0]["id"])
 
     result = [[0 for i in range(len(Cargo_info_array))] for j in range(len(Ship_info_array))]
 
     for i in range(len(Ship_info_array)):
         for j in range(len(Cargo_info_array)):
-            M=Ship_info_array[i]["M_ship"]
-            V=Ship_info_array[i]["V_ship"]
-            m=Cargo_info_array[j]["m_cargo"]
+            M=Ship_info_array[i].weight
+            #V=Ship_info_array[i]["V_ship"]
+            m=Cargo_info_array[j].cargo_weight
 
-            dis_ship_cargo = Loc_Correspond[Ship_info_array[i]["loc"]]-Loc_Correspond[Cargo_info_array[j]["depart"]]
+            # M=Ship_info_array[i]["M_ship"]
+            # V=Ship_info_array[i]["V_ship"]
+            # m=Cargo_info_array[j]["m_cargo"]
+
+            dis_ship_cargo = Loc_Correspond[Ship_info_array[i].depart]-Loc_Correspond[Cargo_info_array[j].depart]
             if dis_ship_cargo < 0:
                 dis_ship_cargo=-dis_ship_cargo
 
-            dis_transport = Loc_Correspond[Cargo_info_array[j]["depart"]]-Loc_Correspond[Cargo_info_array[j]["destin"]]
+            dis_transport = Loc_Correspond[Cargo_info_array[j].depart]-Loc_Correspond[Cargo_info_array[j].destin]
             if dis_transport < 0:
                 dis_transport=-dis_transport
 
-            time = (dis_ship_cargo+dis_transport)/V
-            Cost=Ship_info_array[i]["Cost_trans"]*((M+m)*dis_transport + M*dis_ship_cargo)+ Ship_info_array[i]["Cost_live"]*time
-            Profit=Cargo_info_array[j]["target_money"]-Cost
+            #time = (dis_ship_cargo+dis_transport)/V
+            Cost=Ship_info_array[i]["Cost_trans"]*((M+m)*dis_transport + M*dis_ship_cargo)#+ Ship_info_array[i]["Cost_live"]*time
+            #Profit=Cargo_info_array[j]["target_money"]-Cost
+            Profit=-Cost
 
             result[i][j]=Profit
-
-    #output=pd.DataFrame(data=result[1:], columns=result[0])
-    #output=pd.DataFrame(result[1:], columns=result[0])
-
-    #output.to_csv('D:\\TransportCompetition\\Competition\\water-transport-research\\result.csv', encoding='utf-8')
-
-    db.close_db(mydb)
-
-
-
-
-    #input_data_path = r'D:\TransportCompetition\Competition\water-transport-research\result.csv'
-    #input_data = pd.read_csv(input_data_path, header=None)  # 读取csv文件
-
-    # print(input_data)
-
-    #input_data.head()
-    #X = input_data.values.tolist()  # 将每一行数据转化成一个列表
-    # for i in range(len(X)):
-    #     X[i] = [1]
-    #print(X)
-
-    # graphlist = X
-    #Y = []
-    #for i in X:
-    #    Y.append(i[1:])
 
     graphlist = result
 
@@ -273,50 +227,20 @@ def final_func():
     if ningraphlist_num < 0:  # 若有小于0的值，则整体都变成正数再操作
         for i in range(len(graphlist)):
             for j in range(len(graphlist[i])):
-                # graphlist[i][j] = int(graphlist[i][j]-ningraphlist_num + 1)
                 graphlist[i][j] = int(graphlist[i][j] - ningraphlist_num + 1)
     else:
         for i in range(len(graphlist)):
             for j in range(len(graphlist[i])):
                 graphlist[i][j] = int(graphlist[i][j])
 
-    # print(graphlist)
-
     # 声明数据结构  创建一个Array的数组a用作输入
     a = np.array(graphlist)  # np array with dimension N*N
 
-    #print(a)
-    # cyclenum_path = r'C:\Users\Dell\GAMA_workspace_1\test\includes\cyclenum.csv'
-    # cyclenum = pd.read_csv(cyclenum_path, header=0)  # cyclenum存读取的csv文件
-
-    # Cnum = cyclenum.values.tolist()[0][0]   # cyclenum.values.tolist()是一个列表，二维数组
-    # calout_path = 'C:\\Users\\Dell\\GAMA_workspace_1\\test\\includes\\calout'+str(Cnum)+'.csv'
-    #calout_path = 'D:\\TransportCompetition\\Competition\\water-transport-research\\match_result.csv'
-
-
     # 开始执行
 
-    # if True:
-    # a = np.array([[84, 65, 3, 34], [65, 56, 23, 35], [63, 18, 35, 12]])
-    # a = np.array([[84, 65], [3, 34], [63, 18], [35, 12]])
 
     km = KM()  # 声明对象
     max_ = compute_km(a.copy())  # 使用a的一个副本，copy一个使用   max_是一个一维数组存匹配结果
     return max_
-
-    # print("最大组合:", max_, a[[i[0] for i in max_], [i[1] for i in max_]])
-    # csvout = []
-    # for i in range(len(max_)):
-    #     if max_[i][1] != -1:  # 有最佳匹配，则将匹配结果写入输出的csv二维数组
-    #         csvout.append([max_[i][0], max_[i][1]])
-
-
-    # with open一种文件打开机制，
-    # with open(calout_path, "w", encoding='utf-8', newline="") as f:
-
-    #     writer = csv.writer(f)
-    #     # writer.writerow(["index","a_name"])
-    #     writer.writerows(csvout)
-    # f.close()
 
 # 需要的工作是将这些信息进行计算，之后将权值存到一个文件中去
