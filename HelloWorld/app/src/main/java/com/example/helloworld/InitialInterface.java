aec08496c078b7f48a5c750ca3f0b9ad43934cb6
RealLiuzheng <884712443@qq.com>
2022-03-08 13:58:10 +0800

    Change port selection to spinner.

--- HelloWorld/app/src/main/java/com/example/helloworld/InitialInterface.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/InitialInterface.java
@@ -7,29 +7,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.View;
 import android.widget.TextView;
 
 public class InitialInterface extends AppCompatActivity {
 
-    TextView textView;
+    TextView textView,skip;
     int time=5;
+    int control=0;
     Handler handler=new Handler(){
         @Override
         public void handleMessage(@NonNull Message msg) {
-            if (msg.what==1) {
-                time--;
-                if (time==0) {
-                    //跳转页面
-                    Intent intent=new Intent();
-                    intent.setClass(InitialInterface.this,MainActivity.class);
-                    startActivity(intent);
-                }
-                else{
-                    textView.setText(time+"");//加上一个空字符串，使int变为String
-                    handler.sendEmptyMessageDelayed(1,1000);
-                }
-
+            if (control==1) {
+                ;
             }
+            else
+            {
+                if (msg.what==1) {
+                    time--;
+                    if (time==0) {
+                        //跳转页面
+                        Intent intent=new Intent();
+                        intent.setClass(InitialInterface.this,MainActivity.class);
+                        startActivity(intent);
+                    }
+                    else{
+                        textView.setText(time+"");//加上一个空字符串，使int变为String
+                        handler.sendEmptyMessageDelayed(1,1000);
+                    }
+
+                }
+            }
+
         }
     };
 
@@ -38,7 +47,20 @@ public class InitialInterface extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_interface);
         textView=findViewById(R.id.count_down);
+        skip=findViewById(R.id.skip);
         handler.sendEmptyMessageDelayed(1,1000);
+
+
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent();
+        if(view.getId()==R.id.skip){
+            intent.setClass(InitialInterface.this,MainActivity.class);
+            control=1;
+        }
+
+        startActivity(intent);
 
 
     }
